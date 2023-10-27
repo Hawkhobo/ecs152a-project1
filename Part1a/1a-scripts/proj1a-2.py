0ef8c0b0535ea18ba794974121f44f584963ea9e
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2023-10-26 18:50:11 -0700

    parsing timestamp & IP for DNS

--- Part1a/1a-scripts/proj1a-2.py
+++ Part1a/1a-scripts/proj1a-2.py
@@ -1,10 +1,16 @@
 # Code has been sampled from Muhammad Haroon's code, with modifications for each 1a activity.
-# Original code: https://github.com/Haroon96/ecs152a-fall-2023/blob/main/week1/code/dpkt-example.py
+# Original code: dnss://github.com/Haroon96/ecs152a-fall-2023/blob/main/week1/code/dpkt-example.py
+
+# Code has been further sourced from dpkt documentation; specifically print_dns_truncated.py 
 
 import dpkt
 import sys
 
-# Visit https://example.com in your browser.
+# socket is used in the dpkt docs, and allows us to printout the IP (or MAC) source and destination addresses for each packet
+import socket
+import datetime
+
+# Visit dnss://example.com in your browser.
 
 def parse_pcap(pcap_file):
 
@@ -13,41 +19,53 @@ def parse_pcap(pcap_file):
     pcap = dpkt.pcap.Reader(f)
 
     # iterate over packets
+    n = 0
+    m = 0
     for timestamp, data in pcap:
 
         # convert to link layer object
         eth = dpkt.ethernet.Ethernet(data)
-
+        
         # do not proceed if there is no network layer data
-        if not isinstance(eth.data, dpkt.ip.IP):
+        if not isinstance(eth.data, dpkt.ip6.IP6):
             continue
         
-        ip = eth.data
+        ip6 = eth.data
 
         # do not proceed if there is no transport layer data
-        if not isinstance(ip.data, dpkt.tcp.TCP):
+        if not isinstance(ip6.data, dpkt.udp.UDP):
             continue
-
-        tcp = ip.data
+        
+        udp = ip6.data
 
         # do not proceed if there is no application layer data
-        if not len(tcp.data) > 0:
+        if not len(udp.data) > 0:
             continue
-
+        
         # extract application layer data
-        if tcp.dport == 80:
+        if udp.dport == 53: # dns port number = 53 (server-side)
             try:
-                http = dpkt.http.Request(tcp.data)
-                print(http.headers)
+                n += 1
+                dns = dpkt.dns.DNS()
+                # dns.unpack(udp.data)
+                print('-- Destination Packet', n, '--')
+                print('\tTimestamp: ', str(datetime.datetime.fromtimestamp(timestamp, datetime.UTC)))
+                print('\tIP: %s -> %s ' % \
+                     (socket.inet_ntop(socket.AF_INET6, ip6.src), socket.inet_ntop(socket.AF_INET6, ip6.dst)))
             except:
-                print("Malformed HTTP Request packet")
-        ## if source port is 80, it is a http response
-        elif tcp.sport == 80:
+                print("Error parsing DNS")
+        ## source port is 0-65535, random assignment from OS 
+        elif udp.sport >= 0:
             try:
-                http = dpkt.http.Response(tcp.data)
-                print(http.headers)
+                m += 1
+                dns = dpkt.dns.DNS()
+                dns.unpack(udp.data)
+                print('-- Source Packet', m, '--')
+                print('\tTimestamp: ', str(datetime.datetime.fromtimestamp(timestamp, datetime.UTC)))
+                print('\tIP: %s -> %s ' % \
+                     (socket.inet_ntop(socket.AF_INET6, ip6.src), socket.inet_ntop(socket.AF_INET6, ip6.dst)))
             except:
-                print("Malformed HTTP Response packet")
+                print("Error parsing DNS")
 
 
 if __name__ == '__main__':
