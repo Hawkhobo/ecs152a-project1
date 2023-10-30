57e9d87c9e1f88b4b06b85da844b4e2604fe4de4
krystalchau <krystalchau.20@gmail.com>
2023-10-29 21:13:18 -0700

    1a-4: Complete

--- Part1a/1a-scripts/proj1a-4.py
+++ Part1a/1a-scripts/proj1a-4.py
@@ -4,55 +4,65 @@
 import dpkt
 import sys
 
+# socket is used in the dpkt docs, and allows us to printout the IP (or MAC) source and destination addresses for each packet
+import socket
+import datetime
+
 # Access a FTP server (Type “ftp ftp.gnu.org” in your terminal).
 
 def parse_pcap(pcap_file):
-
+    
     # read the pcap file
     f = open(pcap_file, 'rb')
     pcap = dpkt.pcap.Reader(f)
 
     # iterate over packets
+    n = 0
+    m = 0
     for timestamp, data in pcap:
 
         # convert to link layer object
         eth = dpkt.ethernet.Ethernet(data)
 
         # do not proceed if there is no network layer data
-        if not isinstance(eth.data, dpkt.ip.IP):
+        if not isinstance(eth.data, dpkt.ip6.IP6):
             continue
         
         # extract network layer data
-        ip = eth.data
+        ip6 = eth.data
 
         # do not proceed if there is no transport layer data
-        if not isinstance(ip.data, dpkt.tcp.TCP):
+        if not isinstance(ip6.data, dpkt.tcp.TCP):
             continue
 
         # extract transport layer data
-        tcp = ip.data
+        tcp = ip6.data
 
         # do not proceed if there is no application layer data
-        # here we check length because we don't know protocol yet
         if not len(tcp.data) > 0:
             continue
 
         # extract application layer data
-        ## if destination port is 80, it is a http request
-        if tcp.dport == 80:
+        ## if destination port is 21, it is a FTP request
+        if tcp.dport == 21:
             try:
-                http = dpkt.http.Request(tcp.data)
-                print(http.headers)
+                n+=1
+                print('-- Request Packet', n, '--')
+                print('\tTimestamp: ', str(datetime.datetime.fromtimestamp(timestamp, datetime.UTC)))
+                print('\tSource to Destination: %s -> %s ' % \
+                     (socket.inet_ntop(socket.AF_INET6, ip6.src), socket.inet_ntop(socket.AF_INET6, ip6.dst)))
             except:
-                print("Malformed HTTP Request packet")
-        ## if source port is 80, it is a http response
-        elif tcp.sport == 80:
+                print("Malformed FTP Request packet")
+        ## if source port is 21, it is a FTP response
+        elif tcp.sport == 21:
             try:
-                http = dpkt.http.Response(tcp.data)
-                print(http.headers)
+                m+=1
+                print('-- Response Packet', m, '--')
+                print('\tTimestamp: ', str(datetime.datetime.fromtimestamp(timestamp, datetime.UTC)))
+                print('\tSource to Destination: %s -> %s ' % \
+                     (socket.inet_ntop(socket.AF_INET6, ip6.src), socket.inet_ntop(socket.AF_INET6, ip6.dst)))
             except:
-                print("Malformed HTTP Response packet")
-
+                print("Malformed FTP Response packet")
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
