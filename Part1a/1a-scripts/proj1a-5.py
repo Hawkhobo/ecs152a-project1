43d58a475590066ae6293abbf6fc9e36f006ff5e
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2023-10-30 19:57:05 -0700

    1a-5 refactored for IPv4

--- Part1a/1a-scripts/proj1a-5.py
+++ Part1a/1a-scripts/proj1a-5.py
@@ -4,22 +4,27 @@
 import dpkt
 import sys
 
-# ssh into a CSIF machine.
+# socket is used in the dpkt docs, and allows us to printout the IP (or MAC) source and destination addresses for each packet
+import socket
+import datetime
+
+# Access SDF through SSH (tty.sdf.org is the host)
 
 def parse_pcap(pcap_file):
-
+    
     # read the pcap file
     f = open(pcap_file, 'rb')
     pcap = dpkt.pcap.Reader(f)
 
     # iterate over packets
+    n = 0
     for timestamp, data in pcap:
-
+        n += 1
         # convert to link layer object
         eth = dpkt.ethernet.Ethernet(data)
 
         # do not proceed if there is no network layer data
-        if not isinstance(eth.data, dpkt.ip.IP):
+        if not isinstance(eth.data, dpkt.ip6.IP6) and not  isinstance(eth.data, dpkt.ip.IP):
             continue
         
         # extract network layer data
@@ -33,26 +38,28 @@ def parse_pcap(pcap_file):
         tcp = ip.data
 
         # do not proceed if there is no application layer data
-        # here we check length because we don't know protocol yet
         if not len(tcp.data) > 0:
             continue
 
         # extract application layer data
-        ## if destination port is 80, it is a http request
-        if tcp.dport == 80:
+        ## if destination port is 22, it is a SSH request
+        if tcp.dport == 22:
             try:
-                http = dpkt.http.Request(tcp.data)
-                print(http.headers)
+                print('-- SSH Request Packet', n, '--')
+                print('\tTimestamp: ', str(datetime.datetime.fromtimestamp(timestamp, datetime.UTC)))
+                print('\tSource to Destination: %s -> %s ' % \
+                     (socket.inet_ntop(socket.AF_INET, ip.src), socket.inet_ntop(socket.AF_INET, ip.dst)))
             except:
-                print("Malformed HTTP Request packet")
-        ## if source port is 80, it is a http response
-        elif tcp.sport == 80:
+                print("Malformed SSH Request packet")
+        ## if source port is 22, it is a SSH response
+        elif tcp.sport == 22:
             try:
-                http = dpkt.http.Response(tcp.data)
-                print(http.headers)
+                print('-- SSH Response Packet', n, '--')
+                print('\tTimestamp: ', str(datetime.datetime.fromtimestamp(timestamp, datetime.UTC)))
+                print('\tSource to Destination: %s -> %s ' % \
+                     (socket.inet_ntop(socket.AF_INET, ip.src), socket.inet_ntop(socket.AF_INET, ip.dst)))
             except:
-                print("Malformed HTTP Response packet")
-
+                print("Malformed SSH Response packet")
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
