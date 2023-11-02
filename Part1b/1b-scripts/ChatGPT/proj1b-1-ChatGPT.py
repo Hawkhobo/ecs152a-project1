41cfef4be51bde515dcbc874792c9a77d9fb4f08
krystalchau <krystalchau.20@gmail.com>
2023-11-01 22:28:48 -0700

    Fixed Part 1b's ChatGPT imlementations

--- Part1b/1b-scripts/ChatGPT/proj1b-1-ChatGPT.py
+++ Part1b/1b-scripts/ChatGPT/proj1b-1-ChatGPT.py
@@ -1,34 +1,39 @@
 
-import sys
 import dpkt
+import sys
 import socket
+import datetime
 
-def extract_secrets(pcap_file):
-    with open(pcap_file, 'rb') as f:
-        pcap = dpkt.pcap.Reader(f)
-        for ts, buf in pcap:
-            try:
-                eth = dpkt.ethernet.Ethernet(buf)
-                if isinstance(eth.data, dpkt.ip6.IP6) and isinstance(eth.data.data, dpkt.tcp.TCP):
+def parse_pcap(pcap_file):
+    try:
+        with open(pcap_file, 'rb') as f:
+            pcap = dpkt.pcap.Reader(f)
+            for n, (timestamp, data) in enumerate(pcap, start=1):
+                eth = dpkt.ethernet.Ethernet(data)
+                if isinstance(eth.data, dpkt.ip6.IP6):
                     ip6 = eth.data
-                    tcp = ip6.data
-                    if tcp.dport == 80 or tcp.dport == 443:  # HTTP or HTTPS
-                        packet_repr = repr(tcp.data)
-
-                        # Look for secrets in the string representation (customize this part)
-                        if 'password' in packet_repr or 'secret' in packet_repr:
-                            src_ip = ip6.src
-                            src_port = tcp.sport
-                            dst_ip = ip6.dst
-                            dst_port = tcp.dport
-                            print(f"Secret sent from {src_ip}:{src_port} to {dst_ip}:{dst_port} - {packet_repr}")
-
-            except Exception as e:
-                # Handle exceptions if packet parsing fails
-                pass
+                    if isinstance(ip6.data, dpkt.tcp.TCP) and len(ip6.data.data) > 0:
+                        tcp = ip6.data
+                        if tcp.dport == 80:
+                            try:
+                                print(f'-- HTTP Request, Packet {n} --')
+                                print(f'\tTimestamp: {datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)}')
+                                print(f'\tSource to Destination: {socket.inet_ntop(socket.AF_INET6, ip6.src)} -> {socket.inet_ntop(socket.AF_INET6, ip6.dst)}')
+                                http_request = dpkt.http.Request(tcp.data)
+                                print(f'\tHTTP request: {repr(http_request)}')
+                            except dpkt.dpkt.NeedData:
+                                print('Malformed HTTP Request packet')
+    except FileNotFoundError:
+        print(f"File '{pcap_file}' not found!")
+    except dpkt.dpkt.UnpackError:
+        print("Error unpacking data from the pcap file.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("No pcap file specified!")
     else:
-        extract_secrets(sys.argv[1])
+        parse_pcap(sys.argv[1])
+
+
